53c0eca958766b4460741ea22d529f65f8bcfb08
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2022-02-02 16:34:23 +0100

    Add REGISTRAZIONE AND LOGIN

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,102 @@
+@extends('layouts.main-layout')
+@section('content')
+
+@auth
+<h1>
+    Hello {{Auth::User() -> name}}
+</h1>
+<a class="btn btn-secondary" href="{{route('logout')}}">Logout</a>
+@else
+<h1>
+    If you want to see my site you have to login/register
+</h1>
+@endauth
+    <h2>
+        Registrazione 
+    </h2>
+    <div class="homepage">
+        <div id="registrati">
+            <form method="POST" action="{{ route('register') }}">
+            
+                @method('post')
+                @csrf
+    
+                {{-- name --}}
+                <label for="name">Inserisci il nome</label>
+                <input type="text" name="name" placeholder="name" id="">
+    
+                @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+    
+                {{-- Email --}}
+                <label for="email">Inserisci Email</label>
+                <input type="email" name="email" placeholder="Email">
+    
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                 @enderror
+    
+                {{-- Password --}}
+                <label for="password">Inserisci la password</label>
+                <input type="password" name="password" placeholder="Inserisci password">
+    
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                 @enderror
+    
+    
+                 {{-- conferma password  --}}
+                 <label for="password-confirm">Conferma Password</label>
+                 <input type="password" name="password_confirmation" id="">
+    
+                <input type="submit" value="Invia">
+            </form>
+        </div>
+       
+        {{-- Login --}}
+        <div id="login">
+            <h2>
+                Login
+            </h2>
+            <form method="POST" action="{{ route('login') }}">
+            
+                @method('post')
+                @csrf
+    
+                {{-- Email --}}
+                <label for="email">Inserisci Email</label>
+                <input type="email" name="email" placeholder="Email">
+    
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                 @enderror
+    
+                {{-- Password --}}
+                <label for="password">Inserisci la password</label>
+                <input type="password" name="password" placeholder="Inserisci password">
+    
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                 @enderror
+   
+    
+                <input type="submit" value="Invia">
+            </form>
+        </div>
+    </div>
+
+    
+
+    
+@endsection

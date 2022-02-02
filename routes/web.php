06c76cd07c751b787e0838f89a02f11f9404c941
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2022-02-02 18:49:52 +0100

    Completed

--- routes/web.php
+++ routes/web.php
@@ -25,15 +25,4 @@ Route::post('/login', 'Auth\LoginController@login') -> name('login');
 
 Route::get('/logout', 'Auth\LoginController@logout') -> name('logout');
 
-// maurizio 
 
-
-// Route::get('/', 'Homepage@home') -> name('home');
-
-// Route::post('/register', 'Auth\RegisterController@register') -> name('register');
-// Route::post('/login', 'Auth\LoginController@login') -> name('login');
-
-// Route::get('/logout', 'Auth\LoginController@logout') -> name('logout');
-// Auth::routes();
-
-// Route::get('/home', 'HomeController@index')->name('home');
